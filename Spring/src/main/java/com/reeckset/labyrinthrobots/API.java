1ee598807b6e08980b9b0d8f1aca4d8de8f25f1d
Henrique Lima <henriquelim@hotmail.com>
2019-03-29 23:43:29 +0000

    [API] Finished back-end side

--- Spring/src/main/java/com/reeckset/labyrinthrobots/API.java
+++ Spring/src/main/java/com/reeckset/labyrinthrobots/API.java
@@ -9,17 +9,71 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
 
 @RestController
 public class API {
 
     private Board activeBoard;
 
+    static final Map<String, Function> algorithmToFunction = new HashMap<String, Function>() {{
+        put("DFS", (activeBoard) -> ((Board) activeBoard).dfs_wrapper());
+        put("Iterative Deepening DFS", (activeBoard) -> ((Board) activeBoard).iterativeDFS());
+        put("Iterative Deepening DFS - optimized", (activeBoard) -> ((Board) activeBoard).iterativeDFS(true));
+        put("AStar", (activeBoard) -> ((Board) activeBoard).AStar());
+        put("AStar with non admissable heuristic", (activeBoard) -> ((Board) activeBoard).AStar(true));
+    }};
+
     @CrossOrigin(origins = "http://localhost:8000")
     @RequestMapping(value = "/board", method = RequestMethod.GET)
     public String getBoard(String filePath){
         this.activeBoard = readLevel(filePath);
         return activeBoard.toJSON();
+    }
+
+    @CrossOrigin(origins = "http://localhost:8000")
+    @RequestMapping(value = "/runAlgorithm", method = RequestMethod.GET)
+    public String runAlgorithm(String algorithm){
+
+        long start = System.currentTimeMillis();
+
+        ArrayList<State> solution = (ArrayList<State>) algorithmToFunction.get(algorithm).apply(activeBoard);
+
+        long duration = System.currentTimeMillis() - start;
+
+        return "{\"solution\":" + getSolutionJSON(solution) + ", \"time\":" + duration + "}";
+    }
+
+    @CrossOrigin(origins = "http://localhost:8000")
+    @RequestMapping(value = "/listAlgorithms", method = RequestMethod.GET)
+    public String listAlgorithms(){
+
+        String result = "[";
+
+        int counter = 0;
+        for (String algorithm : algorithmToFunction.keySet()) {
+            result += "\"" + algorithm + "\"";
+            if(counter < algorithmToFunction.size() - 1){
+                result += ", ";
+            }
+            counter++;
+        }
+
+        return result + "]";
+    }
+
+    private static String getSolutionJSON(ArrayList<State> states){
+        String result = "[";
+        for(int i = 0; i < states.size(); i++){
+            result += states.get(i).toJSON();
+            if(i < states.size() - 1){
+                result += ", ";
+            }
+        }
+        return result + "]";
     }
 
     private static Board readLevel(String filePath) {
