527c408546f538ad06ff176cef710634abe7cf53
Henrique Lima <henriquelim@hotmail.com>
2019-03-29 21:48:29 +0000

    added missing api files

---
+++ Spring/src/main/java/com/reeckset/labyrinthrobots/LabyrinthrobotsApplication.java
@@ -0,0 +1,13 @@
+package com.reeckset.labyrinthrobots;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabyrinthrobotsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabyrinthrobotsApplication.class, args);
+    }
+
+}
---
+++ Spring/src/test/java/com/reeckset/labyrinthrobots/LabyrinthrobotsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.reeckset.labyrinthrobots;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LabyrinthrobotsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
