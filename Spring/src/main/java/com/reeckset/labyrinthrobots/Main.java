1dfe75937d7ec8e645638cadab14e71c6f5d1ff4
Angelo Teixeira <imnotteixeira@gmail.com>
2019-03-31 16:54:46 +0100

    Added levels 1-18 in .txt format

--- Spring/src/main/java/com/reeckset/labyrinthrobots/Main.java
+++ Spring/src/main/java/com/reeckset/labyrinthrobots/Main.java
@@ -3,12 +3,29 @@ package com.reeckset.labyrinthrobots;
 
 import com.reeckset.labyrinthrobots.cli.CLI;
 
+import java.io.File;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeoutException;
+
 public class Main {
 
     public static void main(String[] args){
         API api = new API();
         CLI cli = new CLI(api);
         cli.init();
+
+
+//        try {
+//
+//
+//            api.loadLevel(new File("lvl_17.txt"));
+//            Board.printSolution(api.execAlgorithm("AStar", 15).solution);
+//
+//
+//
+//        } catch (InterruptedException | ExecutionException | TimeoutException e) {
+//            e.printStackTrace();
+//        }
     }
 
 }
